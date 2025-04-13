dae16e3aed5627a4f01450f18600a200a30225c5
vientorepublic <markgod0419@gmail.com>
2025-04-13 11:32:51 +0900

    Refactor interaction handling: update interaction menu options and add functionality for toy throwing interaction

--- src/main.c
+++ src/main.c
@@ -9,6 +9,15 @@
 #include "interaction.h"
 #include "store.h"
 
+void openInteractionMenu(int *intimacy, char *catName) {
+    int choice;
+    char input[10];
+    printf("\n상호작용 메뉴: 1. 아무것도 하지 않음 2. 긁어 주기 3. 간식 주기 4. 장난감 던지기\n>> ");
+    fgets(input, sizeof(input), stdin);
+    choice = atoi(input);
+    handleInteraction(&choice, intimacy, catName);
+}
+
 int main() {
 #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);
@@ -30,12 +39,14 @@ int main() {
         moveCat(&catPosition, intimacy, catName);
         renderRoom(catPosition, &soupCount, catName);
 
-        printf("\n어떤 상호작용을 하시겠습니까? 0. 아무것도 하지 않음 1. 긁어 주기 2. 상점 열기 3. 게임 종료\n>> ");
+        printf("\n어떤 것을 하시겠습니까? 1. 상호작용 메뉴 열기 2. 상점 메뉴 열기 3. 게임 종료\n>> ");
         char input[10];
         fgets(input, sizeof(input), stdin);
         choice = atoi(input);
 
-        if (choice == 2) {
+        if (choice == 1) {
+            openInteractionMenu(&intimacy, catName);
+        } else if (choice == 2) {
             openStore(&soupCount, &intimacy);
         } else if (choice == 3) {
             break;
